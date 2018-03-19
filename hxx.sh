bba7a3f143d581bc63606266ff971e4d6a254fec
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-19 22:59:40 +0700

    Create hxx.sh

---
+++ hxx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+output() {
+        printf "\E[0;33;40m"
+        echo $1
+        printf "\E[0m"
+}
+output "";
+output "Upgrade OS"
+sudo apt-get update
+output "Upgrade OS - Done!"
+output "Install CPUminer"
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git screen
+cd /root/
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
